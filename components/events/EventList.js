0d1de49278584549858d1cb8cfc30266cded34a5
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2022-07-29 16:34:02 +0900

    73. 일반적인 React 컴포넌트 추가하기

---
+++ components/events/EventList.js
@@ -0,0 +1,14 @@
+import EventItem from "./EventItem";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
