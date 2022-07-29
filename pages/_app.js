26883e6e3797bd25346629a39cad9ac3e9f7d35d
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2022-07-29 20:14:26 +0900

    78. 일반적인 레이아웃 래퍼 컴포넌트 추가하기

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,9 @@ import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
