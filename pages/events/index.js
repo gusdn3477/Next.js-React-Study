a234957b42d046500a8797896f02a59fe83c6886
gusdn3477 <gusdn3477@naver.com>
2022-07-30 22:40:46 +0900

    81. “iltered Events 페이지를 프로그래밍 방식으로 내비게이팅하기

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/EventSearch";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
